2535aa94e02ba80eac0657718ab1310244a8bf90
Dmitrii Parfenov <parfenovds@protonmail.com>
2023-12-26 21:04:59 +0300

    change training entity to hibernate entity, change its service and repository, change according tests

--- src/main/java/com/epam/service/TrainingService.java
+++ src/main/java/com/epam/service/TrainingService.java
@@ -1,27 +1,13 @@
 package com.epam.service;
 
 import com.epam.entity.Training;
-import com.epam.repository.TrainingRepository;
-import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+import com.epam.repository.BaseRepository;
+import jakarta.validation.Validator;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
-@Log4j2
-public class TrainingService implements BaseService<Training>{
-  private final TrainingRepository trainingRepository;
-  @Override
-  public Training findById(Long id) {
-    Training training = trainingRepository.get(id);
-    log.info("Training acquired from service: " + training);
-    return training;
-  }
-
-  @Override
-  public Training save(Training entity) {
-    Training training = trainingRepository.create(entity);
-    log.info("Training saved in service: " + training);
-    return training;
+public class TrainingService extends BaseServiceImpl<Training>{
+  public TrainingService(BaseRepository<Training> baseRepository, Validator validator) {
+    super(baseRepository, Training.class, validator);
   }
 }
--- src/test/java/com/epam/entity/TrainingTest.java
+++ src/test/java/com/epam/entity/TrainingTest.java
@@ -1,71 +1,38 @@
 package com.epam.entity;
 
 import java.time.LocalDate;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TrainingTest {
 
   @Test
-  public void testTrainingId() {
-    Long expectedId = 1L;
-    Training training = new Training();
-    training.setId(expectedId);
-    Long actualId = training.getId();
-    assertEquals(expectedId, actualId, "Training ID should match");
-  }
+  public void testToString() {
+    // Подготовка данных для теста
+    Trainee trainee = new Trainee();
+    trainee.setId(1L);
 
-  @Test
-  public void testTrainingTraineeId() {
-    Long expectedTraineeId = 2L;
-    Training training = new Training();
-    training.setTraineeId(expectedTraineeId);
-    Long actualTraineeId = training.getTraineeId();
-    assertEquals(expectedTraineeId, actualTraineeId, "Training trainee ID should match");
-  }
+    Trainer trainer = new Trainer();
+    trainer.setId(2L);
 
-  @Test
-  public void testTrainingTrainerId() {
-    Long expectedTrainerId = 3L;
-    Training training = new Training();
-    training.setTrainerId(expectedTrainerId);
-    Long actualTrainerId = training.getTrainerId();
-    assertEquals(expectedTrainerId, actualTrainerId, "Training trainer ID should match");
-  }
+    TrainingType trainingType = new TrainingType();
+    trainingType.setId(3L);
 
-  @Test
-  public void testTrainingName() {
-    String expectedTrainingName = "Java Basics";
-    Training training = new Training();
-    training.setTrainingName(expectedTrainingName);
-    String actualTrainingName = training.getTrainingName();
-    assertEquals(expectedTrainingName, actualTrainingName, "Training name should match");
-  }
+    Training training = Training.builder()
+        .id(100L)
+        .trainee(trainee)
+        .trainer(trainer)
+        .trainingName("TrainingName")
+        .trainingType(trainingType)
+        .trainingDate(LocalDate.of(2023, 12, 31))
+        .trainingDuration(60L)
+        .build();
 
-  @Test
-  public void testTrainingTypeId() {
-    Long expectedTrainingTypeId = 4L;
-    Training training = new Training();
-    training.setTrainingTypeId(expectedTrainingTypeId);
-    Long actualTrainingTypeId = training.getTrainingTypeId();
-    assertEquals(expectedTrainingTypeId, actualTrainingTypeId, "Training type ID should match");
-  }
+    // Ожидаемая строка
+    String expectedString = "Training{id=100, traineeId=1, trainerId=2, trainingName='TrainingName', " +
+        "trainingType=TrainingType(id=3, trainingTypeName=null), trainingDate=2023-12-31, trainingDuration=60}";
 
-  @Test
-  public void testTrainingDate() {
-    LocalDate expectedTrainingDate = LocalDate.of(2023, 12, 1);
-    Training training = new Training();
-    training.setTrainingDate(expectedTrainingDate);
-    LocalDate actualTrainingDate = training.getTrainingDate();
-    assertEquals(expectedTrainingDate, actualTrainingDate, "Training date should match");
-  }
-
-  @Test
-  public void testTrainingDuration() {
-    Long expectedTrainingDuration = 60L;
-    Training training = new Training();
-    training.setTrainingDuration(expectedTrainingDuration);
-    Long actualTrainingDuration = training.getTrainingDuration();
-    assertEquals(expectedTrainingDuration, actualTrainingDuration, "Training duration should match");
+    // Проверка на соответствие ожидаемой строке
+    assertEquals(expectedString, training.toString());
   }
 }
